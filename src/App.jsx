77774770d3756dda2386d53c04c31fd705fca94c
annako-io <annako1@icloud.com>
2023-07-25 18:34:47 -0700

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { createRoot } from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Pet Adoption Time!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Pet Adoption Time!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
